6b918a5b245a364ed5acdec8875860d9dc88e839
Alexander Pope <alex@pope-industries.com>
2017-09-14 09:10:20 +0200

    add sleep()

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 
 module.exports = {
   testServer: require('./lib/server'),
-  mockServiceWorker: require('sw-test-env')
+  mockServiceWorker: require('sw-test-env'),
+  sleep
 };
+
+function sleep(duration) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, duration);
+  });
+}
